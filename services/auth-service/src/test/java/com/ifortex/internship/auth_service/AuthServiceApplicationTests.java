96410541ffe243923bbc12230fec9664246e6c02
AndreySemeynikovIF <andrey.semeynikov@ifortex.com>
2024-12-12 12:05:17 +0300

    init commit

---
+++ services/auth-service/src/test/java/com/ifortex/internship/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifortex.internship.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
